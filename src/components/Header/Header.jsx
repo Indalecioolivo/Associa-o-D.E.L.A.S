94a6d6e008ae0ad10ce7d25d39022302a9fb2394
Indalecio Olivo <indalecioolivo@gmail.com>
2024-12-10 21:05:55 -0300

    feat: header criado, estilização responsiva NÃO finalizada

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,34 @@
 import "./Header.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import MenuImg from "../../assets/sandwich.png";
 
 export default function Header() {
   return (
-    <header>
-      <img src={MenuImg} alt="" />
-      <p>Associação DELAS</p>
+    <header className="header-container">
+      <nav>
+        <img
+          src={MenuImg}
+          alt=""
+          className="mobile-menu"
+          onClick={(e) => handleClickMenu(e)}
+        />
+        <a href="/" className="logo">
+          Associação D.E.L.A.S
+        </a>
+        <ul className="nav-list">
+          <li>
+            <Link to="/">
+              <a href="">Página Inicial</a>
+            </Link>
+          </li>
+          <li>
+            <Link to="/events">
+              <a href="/events">Eventos</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
